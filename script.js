60038c2676e570ade10b638dff2938c95d180572
Ekjot Singh <ekjotsingh00776@gmail.com>
2025-04-01 14:36:26 -0700

    added click functionality on list and cross

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ function addTask() {
     return span;
   }
 }
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
