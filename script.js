bd0bf49ba7720297a60ee4b9ce826e221e9ebb05
Ekjot Singh <ekjotsingh00776@gmail.com>
2025-04-01 14:25:01 -0700

    added cross span to list items

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ function addTask() {
   if (inputBox.value === "") {
     alert("You must write something");
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+    let li = getTaskFromInputBox();
     listContainer.appendChild(li);
+
+    li.appendChild(getCross());
   }
   inputBox.value = "";
+
+  function getTaskFromInputBox() {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    return li;
+  }
+  function getCross() {
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    return span;
+  }
 }
